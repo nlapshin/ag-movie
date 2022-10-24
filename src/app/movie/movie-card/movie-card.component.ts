20d15a82a8fe557ee2c96d2b8586580bd32e34de
Лапшин Николай <lapshin_n@itstep.loc>
2022-10-24 18:59:07 +0300

    New commit

---
+++ src/app/movie/movie-card/movie-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.css']
+})
+export class MovieCardComponent {
+  movie: any = null
+}
--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -2,14 +2,6 @@ import { Component } from '@angular/core';
 
 import { MovieService } from './movie.service';
 
-// Как подключить стили?
-// 1. Создавть файл movie.component.css
-// в той же папке
-// 2. Прописать в комопненте
-// styleUrls: ['./movie.component.css']
-// 3. Перезапустить сервер
-// npm run start
-
 @Component({
   selector: 'movie',
   templateUrl: './movie.component.html',
